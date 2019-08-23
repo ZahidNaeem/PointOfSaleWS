0528edfd9207499c253701d47e4316bd0b7c5218
ZahidNaeem <hzahidnaeem@gmail.com>
2019-08-23 15:37:20 +0500

    Code added to test how multiple custom mappings with same datatypes will be performed

--- src/main/java/org/zahid/apps/web/pos/mapper/ItemStockMapper.java
+++ src/main/java/org/zahid/apps/web/pos/mapper/ItemStockMapper.java
@@ -2,9 +2,7 @@ package org.zahid.apps.web.pos.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.zahid.apps.web.pos.dto.ItemStockDTO;
-import org.zahid.apps.web.pos.entity.Item;
 import org.zahid.apps.web.pos.entity.ItemStock;
 import org.zahid.apps.web.pos.service.ItemService;
 
@@ -16,4 +14,36 @@ public interface ItemStockMapper {
 
   @Mapping(target = "item", expression = "java(itemStock.getItem().getItemCode())")
   ItemStockDTO ItemStockToItemStockDTO(final ItemStock itemStock);
+
+  // Below commented code was added to test how multiple custom mappings with same datatypes will be performed
+
+  /*
+  @Mapping(target = "item2", source = "dto.item2", qualifiedByName = "diItem2")
+  @Mapping(target = "item3", source = "dto.item3", qualifiedByName = "diItem3")
+  ItemStock ItemStockDTOToItemStock(final ItemStockDTO dto, final ItemService itemService);
+
+  @Mapping(target = "item2", source = "itemStock.item2", qualifiedByName = "idItem2")
+  @Mapping(target = "item3", source = "itemStock.item3", qualifiedByName = "idItem3")
+  ItemStockDTO ItemStockToItemStockDTO(final ItemStock itemStock);
+
+  @Named("diItem2")
+  default Item mapItem2(Long value) {
+    return null;
+  }
+
+  @Named("diItem3")
+  default Item mapItem3(Long value) {
+    return null;
+  }
+
+  @Named("idItem2")
+  default Long mapItem2(Item value) {
+    return null;
+  }
+
+  @Named("idItem3")
+  default Long mapItem3(Item value) {
+    return null;
+  }
+  */
 }
