a112a4156e1f89aff06908d8bfc416e3b8ece233
ZahidNaeem <hzahidnaeem@gmail.com>
2019-10-09 18:54:32 +0500

    logging file appender configuration set

--- src/main/java/org/zahid/apps/web/pos/PointOfSaleApplication.java
+++ src/main/java/org/zahid/apps/web/pos/PointOfSaleApplication.java
@@ -1,5 +1,8 @@
 package org.zahid.apps.web.pos;
 
+import org.apache.commons.logging.impl.LogKitLogger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,8 +11,12 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 public class PointOfSaleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PointOfSaleApplication.class, args);
-    }
+  private static final Logger LOG = LogManager.getLogger(PointOfSaleApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(PointOfSaleApplication.class, args);
+    LOG.debug("==============================================Debug===========================================");
+    LOG.info("==============================================info===========================================");
+  }
 
 }
